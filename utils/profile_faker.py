f008758e29344dd56ed7747629886e79b8ee4109
Lam <LamTrang@abedu.vn>
2025-04-28 15:14:34 +0700

    a

--- utils/profile_faker.py
+++ utils/profile_faker.py
@@ -1,26 +1,32 @@
 from faker import Faker
+import random
 
 def generate_profile(locale='en_US'):
     fake = Faker(locale)
-    profile = fake.profile()
-    parent = fake.profile()
+    gender = random.choice(['male', 'female'])
+    first_name = fake.first_name_male() if gender == 'male' else fake.first_name_female()
+    last_name = fake.last_name()
+    full_name = f"{first_name} {last_name}"
+    parent_last_name = last_name
+    father_first = fake.first_name_male()
     return {
-        'first_name': profile['name'].split()[0],
-        'last_name': profile['name'].split()[-1],
-        'name': profile['name'],
-        'gender': profile['sex'],
-        'dob': profile['birthdate'].isoformat(),
-        'email': profile['mail'],
+        'first_name': first_name,
+        'last_name': last_name,
+        'name': full_name,
+        'gender': gender,
+        'dob': fake.date_of_birth(minimum_age=18, maximum_age=50).isoformat(),
+        'email': fake.email(),
         'cell_phone': fake.phone_number(),
         'phone': fake.phone_number(),
-        'ssn': fake.ssn(),
+        'ssn': fake.ssn() if hasattr(fake, 'ssn') else 'N/A',
         'street': fake.street_address(),
         'city': fake.city(),
-        'country': fake.country(),
+        'state': fake.state(),
+        'country': fake.current_country() if hasattr(fake, 'current_country') else 'N/A',
         'zip_code': fake.postcode(),
         'parent': {
-            'first_name': parent['name'].split()[0],
-            'last_name': parent['name'].split()[-1],
-            'email': parent['mail']
+            'first_name': father_first,
+            'last_name': parent_last_name,
+            'email': fake.email()
         }
     }
