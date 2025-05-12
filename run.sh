da291ab0a94dee07bd85b5b14801abc5118b0b3d
lampv <lampv01203@gmail.com>
2025-05-13 03:51:50 +0700

    a

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ cd /opt/hopehub/profile_faker_service
 git pull
 pip3 install -r requirements.txt
 pkill -f "5002"
-nohup gunicorn -w 2 -b 127.0.0.1:5002 app:app > log.txt 2>&1 &
+nohup gunicorn -w 2 -b 0.0.0.0:5002 app:app > log.txt 2>&1 &
